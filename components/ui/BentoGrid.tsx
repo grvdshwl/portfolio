1f9b5e0bff6baebc6db4c77e5206d818d06ec8e1
Gaurav Deshwal <gauravd@geekyants.com>
2024-07-20 18:58:09 +0530

    Changed the email for clipboard

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -65,7 +65,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "grv.dshwl.18@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
